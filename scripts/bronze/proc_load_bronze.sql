f7487f83bca10b05f54cd973ad19a6eb8eb4efbc
Axel Lucas Gómez Caamaño <gomezaxel.lucas@gmail.com>
2026-02-06 09:23:10 -0300

    changes

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,77 +1,110 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	BEGIN TRY
+		DECLARE @start_time DATETIME, @end_time DATETIME, @total_time INT;
+		PRINT '===============================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '===============================================================';
 
-	PRINT '===============================================================';
-	PRINT 'Loading Bronze Layer';
-	PRINT '===============================================================';
+		PRINT '---------------------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '---------------------------------------------------------------';
 
-	PRINT '===============================================================';
-	PRINT 'Loading CRM Tables';
-	PRINT '===============================================================';
+		SET @start_time = GETDATE();
 
-	PRINT '>> Truncating Table: bronze.crm_cust_info';
-	TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
 
-	PRINT '>> Inserting Data Into: bronze.crm_cust_info';
-	BULK INSERT bronze.crm_cust_info
-	FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,          -- Skip the first row, header
-		FIELDTERMINATOR = ',', -- Fields terminated by ,
-		TABLOCK                -- Ensure better performance by locking destination table
-	);
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,          -- Skip the first row, header
+			FIELDTERMINATOR = ',', -- Fields terminated by ,
+			TABLOCK                -- Ensure better performance by locking destination table
+		);
 
-	TRUNCATE TABLE bronze.crm_prd_info;
+		TRUNCATE TABLE bronze.crm_prd_info;
 
-	BULK INSERT bronze.crm_prd_info
-	FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,          -- Skip the first row, header
-		FIELDTERMINATOR = ',', -- Fields terminated by ,
-		TABLOCK                -- Ensure better performance by locking destination table
-	);
+		BULK INSERT bronze.crm_prd_info
+		FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,          -- Skip the first row, header
+			FIELDTERMINATOR = ',', -- Fields terminated by ,
+			TABLOCK                -- Ensure better performance by locking destination table
+		);
 
-	TRUNCATE TABLE bronze.crm_sales_details;
+		TRUNCATE TABLE bronze.crm_sales_details;
 
-	BULK INSERT bronze.crm_sales_details
-	FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,          -- Skip the first row, header
-		FIELDTERMINATOR = ',', -- Fields terminated by ,
-		TABLOCK                -- Ensure better performance by locking destination table
-	);
+		BULK INSERT bronze.crm_sales_details
+		FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,          -- Skip the first row, header
+			FIELDTERMINATOR = ',', -- Fields terminated by ,
+			TABLOCK                -- Ensure better performance by locking destination table
+		);
 
-	PRINT '===============================================================';
-	PRINT 'Loading CRM Tables';
-	PRINT '===============================================================';
+		SET @end_time = GETDATE();
 
-	TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------------';
 
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-	WITH (
-		FIRSTROW = 2,          -- Skip the first row, header
-		FIELDTERMINATOR = ',', -- Fields terminated by ,
-		TABLOCK                -- Ensure better performance by locking destination table
-	);
+		PRINT '---------------------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '---------------------------------------------------------------';
 
-	TRUNCATE TABLE bronze.erp_loc_a101;
+		SET @total_time = DATEDIFF(second, @start_time, @end_time);
 
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-	WITH (
-		FIRSTROW = 2,          -- Skip the first row, header
-		FIELDTERMINATOR = ',', -- Fields terminated by ,
-		TABLOCK                -- Ensure better performance by locking destination table
-	);
+		SET @start_time = GETDATE();
+		
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
 
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,          -- Skip the first row, header
+			FIELDTERMINATOR = ',', -- Fields terminated by ,
+			TABLOCK                -- Ensure better performance by locking destination table
+		);
 
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-	WITH (
-		FIRSTROW = 2,          -- Skip the first row, header
-		FIELDTERMINATOR = ',', -- Fields terminated by ,
-		TABLOCK                -- Ensure better performance by locking destination table
-	);
+		TRUNCATE TABLE bronze.erp_loc_a101;
+
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		WITH (
+			FIRSTROW = 2,          -- Skip the first row, header
+			FIELDTERMINATOR = ',', -- Fields terminated by ,
+			TABLOCK                -- Ensure better performance by locking destination table
+		);
+
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'D:\Cursos\SQL\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH (
+			FIRSTROW = 2,          -- Skip the first row, header
+			FIELDTERMINATOR = ',', -- Fields terminated by ,
+			TABLOCK                -- Ensure better performance by locking destination table
+		);
+		
+		SET @end_time = GETDATE();
+
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------------';
+
+		SET @total_time = @total_time + DATEDIFF(second, @start_time, @end_time);
+		PRINT '>> Total Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------------';
+
+	END TRY
+	BEGIN CATCH
+		PRINT '===============================================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '===============================================================';
+	END CATCH
 END
